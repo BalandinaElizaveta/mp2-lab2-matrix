11a996d30c83659f57221472a919052cd568d403
BalandinaElizaveta <balandina_liza@mail.ru>
2016-11-29 10:26:29 +0300

    Update test_tmatrix.cpp

--- test/test_tmatrix.cpp
+++ test/test_tmatrix.cpp
@@ -9,33 +9,33 @@
 TEST(TMatrix, can_create_matrix_with_positive_length)
 {
 	ASSERT_NO_THROW(TMatrix<int> m(5));
-	//ASSERT_NO_THROW(TMatrix<int> m(2));
+	
 }
 
 TEST(TMatrix, cant_create_too_large_matrix)
 {
 	ASSERT_ANY_THROW(TMatrix<int> m(MAX_MATRIX_SIZE + 1));
-	//ASSERT_ANY_THROW(TMatrix<int> m(MAX_MATRIX_SIZE + 1));
+	
 }
 
 TEST(TMatrix, throws_when_create_matrix_with_negative_length)
 {
 	ASSERT_ANY_THROW(TMatrix<int> m(-5));
-	//ASSERT_ANY_THROW(TMatrix<int> m(-5));
+	
 }
 
 TEST(TMatrix, can_create_copied_matrix)
 {
 	TMatrix<int> m(5);
-	//TMatrix<int> m(5);
+	
 
 	ASSERT_NO_THROW(TMatrix<int> m1(m));
-	//ASSERT_NO_THROW(TMatrix<int> m1(m));
+	
 }
 
 TEST(TMatrix, copied_matrix_is_equal_to_source_one)
 {
-	//ADD_FAILURE();
+	
 	TMatrix<int> Matrix(4);
 	Matrix[1][1] = 2;
 	TMatrix<int> Matrix1(Matrix);
@@ -45,7 +45,7 @@ TEST(TMatrix, copied_matrix_is_equal_to_source_one)
 
 TEST(TMatrix, copied_matrix_has_its_own_memory)
 {
-	//ADD_FAILURE();
+	
 	TMatrix<int> Matrix(4);
 
 	Matrix[1][1] = 1;
@@ -59,7 +59,7 @@ TEST(TMatrix, copied_matrix_has_its_own_memory)
 
 TEST(TMatrix, can_get_size)
 {
-	//ADD_FAILURE();
+	
 	int size = 4;
 
 	TMatrix<int> Matrix(size);
@@ -71,7 +71,7 @@ TEST(TMatrix, can_get_size)
 
 TEST(TMatrix, can_set_and_get_element)
 {
-	//ADD_FAILURE();
+	
 	int res = 2;
 	TMatrix<int> Matrix(4);
 
@@ -82,21 +82,21 @@ TEST(TMatrix, can_set_and_get_element)
 
 TEST(TMatrix, throws_when_set_element_with_negative_index)
 {
-	//ADD_FAILURE();
+	
 	TMatrix<int> Matrix;
 	ASSERT_ANY_THROW(Matrix[-2]);
 }
 
 TEST(TMatrix, throws_when_set_element_with_too_large_index)
 {
-	//ADD_FAILURE();
+	
 	TMatrix<int> Matrix;
 	ASSERT_ANY_THROW(Matrix[MAX_MATRIX_SIZE + 1]);
 }
 
 TEST(TMatrix, can_assign_matrix_to_itself)
 {
-	//ADD_FAILURE();
+	
 	TMatrix<int> Matrix(4);
 
 	ASSERT_NO_THROW(Matrix = Matrix);
@@ -104,7 +104,7 @@ TEST(TMatrix, can_assign_matrix_to_itself)
 
 TEST(TMatrix, can_assign_matrices_of_equal_size)
 {
-	//ADD_FAILURE();
+	
 	int size = 4;
 
 	TMatrix<int> Matrix(size);
@@ -117,7 +117,7 @@ TEST(TMatrix, can_assign_matrices_of_equal_size)
 
 TEST(TMatrix, assign_operator_change_matrix_size)
 {
-	//ADD_FAILURE();
+	
 	int size = 4; int size1 = size + 2;
 	TMatrix<int> Matrix(size);
 	TMatrix<int> Matrix1(size1);
@@ -129,7 +129,7 @@ TEST(TMatrix, assign_operator_change_matrix_size)
 
 TEST(TMatrix, can_assign_matrices_of_different_size)
 {
-	//ADD_FAILURE();
+	
 	int size = 4;
 
 	TMatrix<int> Matrix(size);
@@ -142,7 +142,7 @@ TEST(TMatrix, can_assign_matrices_of_different_size)
 
 TEST(TMatrix, compare_equal_matrices_return_true)
 {
-	//ADD_FAILURE();
+	
 	int size = 4;
 
 	TMatrix<int> Matrix(size);
@@ -153,7 +153,7 @@ TEST(TMatrix, compare_equal_matrices_return_true)
 
 TEST(TMatrix, compare_matrix_with_itself_return_true)
 {
-	//ADD_FAILURE();
+	
 	int size = 4;
 
 	TMatrix<int> Matrix(size);
@@ -163,7 +163,7 @@ TEST(TMatrix, compare_matrix_with_itself_return_true)
 
 TEST(TMatrix, matrices_with_different_size_are_not_equal)
 {
-	//ADD_FAILURE();
+	
 	int size = 4;
 
 	TMatrix<int> Matrix(size);
@@ -174,7 +174,7 @@ TEST(TMatrix, matrices_with_different_size_are_not_equal)
 
 TEST(TMatrix, can_add_matrices_with_equal_size)
 {
-	//ADD_FAILURE();
+	
 	int size = 2;
 
 	TMatrix<int> tmp(size);
@@ -202,7 +202,7 @@ TEST(TMatrix, can_add_matrices_with_equal_size)
 
 TEST(TMatrix, cant_add_matrices_with_not_equal_size)
 {
-	//ADD_FAILURE();
+	
 	int size = 2; int size1 = size + 1;
 
 	TMatrix<int> Matrix1(size);
@@ -213,7 +213,7 @@ TEST(TMatrix, cant_add_matrices_with_not_equal_size)
 
 TEST(TMatrix, can_subtract_matrices_with_equal_size)
 {
-	//ADD_FAILURE();
+	
 	int size = 2;
 
 	TMatrix<int> tmp(size);
